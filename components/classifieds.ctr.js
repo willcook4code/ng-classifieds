a81091b4faa6952ba74b027b4fccb6ad19ffcfd9
Will Cook <wellsee83@gmail.com>
2016-08-15 12:30:51 -0500

    Toolbar and Item Card

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,11 +3,10 @@
 	angular
 		.module("ngClassifieds")
 		.controller("classfiedsCtrl", function($scope) {
-			$scope.name = {
-				first: "Will",
-				last: "Cook"
-			};
-
-			$scope.message = "Hello, world!!!"
+			$scope.classified = {
+				title: "First Item",
+				price: "$1,000,000",
+				description: "Lorem ipsum Dolor Duis dolore cupidatat laboris mollit dolor labore cupidatat proident eiusmod velit aliquip aute officia sunt occaecat occaecat dolore nulla incididunt occaecat quis amet voluptate aute Ut reprehenderit ut reprehenderit est in do reprehenderit qui nulla enim cillum adipisicing id officia nostrud sunt labore in sint mollit eu nulla magna veniam Excepteur cupidatat officia ex et sit anim esse enim reprehenderit tempor elit laborum dolore in sunt velit Duis exercitation ullamco veniam veniam nisi aliqua cillum ex cupidatat culpa sint commodo do est sint ex dolor est laboris velit dolore veniam fugiat fugiat nostrud tempor non laborum sunt laboris amet aliqua sit deserunt consectetur sed voluptate est enim dolor adipisicing tempor ut officia ea reprehenderit in est dolore in minim magna fugiat ut pariatur sed sint aliqua laboris ad reprehenderit minim culpa anim velit enim occaecat dolor."
+			}
 		});
 })();
