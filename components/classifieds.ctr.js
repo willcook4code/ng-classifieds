c896e69d69fddf2d63db701814d360202febc7c6
Will Cook <wellsee83@gmail.com>
2016-08-15 16:22:36 -0500

    Adding new classified functionality

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -2,16 +2,35 @@
 	"use strict";
 	angular
 		.module("ngClassifieds")
-		.controller("classfiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav) {
+		.controller("classfiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
 
 			classifiedsFactory.getClassifieds().then(function(classifieds) {
 				$scope.classifieds = classifieds.data;
 			});
+			var contact = {
+				name: "Will Cook",
+				phone: "(545) 888-9999",
+				email: "will@will.com "
+			}
 			$scope.openSidebar = function() {
 				$mdSidenav('left').open();
 			}
 			$scope.closeSidebar = function() {
 				$mdSidenav('left').close();
+			}
+			$scope.saveClassified = function(classified) {
+				if (classified) {
+					classified.contact = contact;
+					$scope.classifieds.push(classified);
+					$scope.classified = {};
+					$scope.closeSidebar();
+					$mdToast.show(
+						$mdToast.simple()
+							.content("Classified Saved!")
+							.position("top, right")
+							.hideDelay(3000)
+					);
+				}
 			}
 		});
 })();
