03ad01dc450510d91fe41072dabe96391b5019d9
Will Cook <wellsee83@gmail.com>
2016-08-15 16:39:09 -0500

    Editing classifieds setup

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -24,13 +24,28 @@
 					$scope.classifieds.push(classified);
 					$scope.classified = {};
 					$scope.closeSidebar();
-					$mdToast.show(
-						$mdToast.simple()
-							.content("Classified Saved!")
-							.position("top, right")
-							.hideDelay(3000)
-					);
+					showToast("Classified Saved!");
 				}
+			}
+			$scope.editClassified = function(classified) {
+				$scope.editing = true;
+				$scope.openSidebar();
+				$scope.classified = classified;
+			}
+			$scope.saveEdit = function() {
+				$scope.editing = false;
+				$scope.classified = {};
+				$scope.closeSidebar();
+				showToast("Edit Saved!");
+			}
+
+			function showToast(message) {
+				$mdToast.show(
+					$mdToast.simple()
+						.content(message)
+						.position("top, right")
+						.hideDelay(3000)
+				);
 			}
 		});
 })();
